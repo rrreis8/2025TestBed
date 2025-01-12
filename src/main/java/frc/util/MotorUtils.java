d3c5e484b1bb00ef8d2478eca42df262a077d455
SethCush <sethcushman17@gmail.com>
2025-01-12 13:48:43 -0500

    Added utils for math (including some swerve stuff) and motors

---
+++ src/main/java/frc/util/MotorUtils.java
@@ -0,0 +1,46 @@
+package frc.util;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.PowerDistribution;
+import edu.wpi.first.wpilibj.Timer;
+
+/*
+ *   MotorStall object should be instantiated in init() method of a command
+ *   isFinished() should call isStalled() to determine if stalled for longer than allowed
+ */
+public class MotorUtils {
+	public static final double DEFAULT_TIMEOUT = 0.15;
+	private double timeout;
+	private double time;
+	final int PDPChannel;
+	final double currentThreshold;
+	private PowerDistribution pdp;
+
+	public MotorUtils(PowerDistribution pdp, int PDPPort, double currentThreshold) {
+		this.pdp = pdp;
+		this.timeout = DEFAULT_TIMEOUT;
+		this.PDPChannel = PDPPort;
+		this.currentThreshold = currentThreshold;
+		time = Timer.getFPGATimestamp();
+	}
+
+	public MotorUtils(PowerDistribution pdp, int PDPPort, double currentThreshold, double timeout) {
+		this(pdp, PDPPort, currentThreshold);
+		this.timeout = timeout;
+	}
+
+	public boolean isStalled() {
+		final double currentValue = pdp.getCurrent(PDPChannel);
+		final double now = Timer.getFPGATimestamp();
+
+		if (currentValue < currentThreshold) {
+			time = now;
+		} else {
+			DriverStation.reportError("Motor stall, PDP Channel=" + PDPChannel, false);
+			if (now - time > timeout) {
+				return true;
+			}
+		}
+		return false;
+	}
+}
---
+++ src/main/java/frc/util/math/swerve/CentricMode.java
@@ -0,0 +1,7 @@
+package frc.util.math.swerve;
+
+public enum CentricMode {
+    ROBOT,
+    ROBOT_SLOW,
+    FIELD
+}
