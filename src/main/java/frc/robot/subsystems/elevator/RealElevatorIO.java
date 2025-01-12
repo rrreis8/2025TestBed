d9c278a602ce099b99d183585b1e442c64ab6b63
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-12 11:04:46 -0500

    Replaced Talon with 2 Neos (they bugged their documentation) and also made follower motor.

--- src/main/java/frc/robot/subsystems/elevator/RealElevatorIO.java
+++ src/main/java/frc/robot/subsystems/elevator/RealElevatorIO.java
@@ -1,21 +1,24 @@
 package frc.robot.subsystems.elevator;
 
 
-import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkLowLevel;
 import frc.robot.constants.Constants;
 
 public class RealElevatorIO implements ElevatorIO{
-    public final WPI_TalonSRX elevatorMotor;
+    public final SparkMax elevatorMotor1;
+    public final SparkMax elevatorMotor2;
     public RealElevatorIO(){
-        this.elevatorMotor = new WPI_TalonSRX(Constants.ELEVATOR_MOTOR_ID);
+        this.elevatorMotor1 = new SparkMax(Constants.ELEVATOR_MOTOR_1_ID, SparkLowLevel.MotorType.kBrushless);
+        this.elevatorMotor2 = new SparkMax(Constants.ELEVATOR_MOTOR_2_ID, SparkLowLevel.MotorType.kBrushless);
     }
     @Override
     public void setSpeed(double spd){
-        elevatorMotor.set(spd);
+        elevatorMotor1.set(spd);
+        elevatorMotor2.set(spd);
     }
     @Override
     public void updateInputs(ElevatorInputs inputs) {
-        inputs.elevatorMotorPosition = elevatorMotor.getSelectedSensorPosition();
+        inputs.elevatorMotorPosition = elevatorMotor1.getEncoder().getPosition();
     }
 }
