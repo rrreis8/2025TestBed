51ddaa9fdf2583375033a1dfe486622c43166475
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-17 13:46:18 -0500

    added some stuff

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "FRC2025_Java";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 13;
+  public static final String GIT_SHA = "e675efca4e27b286e3799d8cb9072bca9049efab";
+  public static final String GIT_DATE = "2025-01-13 20:22:18 EST";
+  public static final String GIT_BRANCH = "LS_PathPlanner_3";
+  public static final String BUILD_DATE = "2025-01-16 18:21:01 EST";
+  public static final long BUILD_UNIX_TIME = 1737069661088L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants(){}
+}
