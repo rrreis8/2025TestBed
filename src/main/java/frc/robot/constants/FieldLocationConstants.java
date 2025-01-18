7ed79bbd6be46b44f7e034c813f64ed496994f3a
Lev Strougov <62769580+Levercpu@users.noreply.github.com>
2025-01-17 22:44:39 -0500

    added constants file for quick reference of locations

---
+++ src/main/java/frc/robot/constants/FieldLocationConstants.java
@@ -0,0 +1,42 @@
+package frc.robot.constants;
+
+public class FieldLocationConstants {
+    public static final double FIELD_LENGTH = 48.0;
+    public static final double FIELD_WIDTH = 24.0;
+    public static final double FIELD_CENTER_X = FIELD_LENGTH / 2.0;
+    public static final double FIELD_CENTER_Y = FIELD_WIDTH / 2.0;
+    public static final double START_CENTER_X = 0.0;
+    public static final double START_CENTER_Y = 0.0;
+    public static final double START_CENTER_ANGLE = 0.0;
+    public static final double START_SIDE_UP_X = 0.0;
+    public static final double START_SIDE_UP_Y = 0.0;
+    public static final double START_SIDE_UP_ANGLE = 0.0;
+    public static final double START_SIDE_DOWN_X = 0.0;
+    public static final double START_SIDE_DOWN_Y = 0.0;
+    public static final double START_SIDE_DOWN_ANGLE = 0.0;
+    public static final double REEF_A_X = 0.0;
+    public static final double REEF_A_Y = 0.0;
+    public static final double REEF_A_ANGLE = 0.0;
+    public static final double REEF_B_X = 0.0;
+    public static final double REEF_B_Y = 0.0;
+    public static final double REEF_B_ANGLE = 0.0;
+    public static final double REEF_C_X = 0.0;
+    public static final double REEF_C_Y = 0.0;
+    public static final double REEF_C_ANGLE = 0.0;
+    public static final double REEF_D_X = 0.0;
+    public static final double REEF_D_Y = 0.0;
+    public static final double REEF_D_ANGLE = 0.0;
+    public static final double REEF_E_X = 0.0;
+    public static final double REEF_E_Y = 0.0;
+    public static final double REEF_E_ANGLE = 0.0;
+    public static final double REEF_F_X = 0.0;
+    public static final double REEF_F_Y = 0.0;
+    public static final double REEF_F_ANGLE = 0.0;
+    public static final double SOURCE_1_X = 0.0;
+    public static final double SOURCE_1_Y = 0.0;
+    public static final double SOURCE_1_ANGLE = 0.0;
+    public static final double SOURCE_2_X = 0.0;
+    public static final double SOURCE_2_Y = 0.0;
+    public static final double SOURCE_2_ANGLE = 0.0;
+    //TODO: change these all later
+}
